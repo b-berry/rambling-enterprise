ecf8a91cf8c4577296faac89387af8e3effe0977
Bryan Berry <bryan@endpoint.com>
2017-03-20 14:56:41 -0700

    Added initial NET::HTTP evaluation

--- getHttpTime.rb
+++ getHttpTime.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'benchmark'
+require 'net/http'
 require 'optparse'
 require 'ostruct'
 require 'pry'
@@ -24,7 +26,7 @@ class Optparse
 
             opts.on("-u", "--url URL", 
                 "Specify Host URL       default: #{options.url}") do |url|
-                options.host = url
+                options.url = url
             end
             
             opts.on("-d", "--duration",
@@ -44,5 +46,73 @@ class Optparse
 
 end
 
+def httpGet(uri)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme = 'https'
+        #http.use_ssl = true
+        #http.ssl_version = :SSLv3
+    end
+
+    #Net::HTTP.start(uri.host, uri.port).start do |http|
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+
+    #end
+
+    return response 
+
+end
+
+
+def runTest(options, uri)
+
+    # Run Benchmark over GET request
+    time = Benchmark.measure do
+
+        $response = httpGet(uri)
+
+    end    
+
+    return $response
+
+end
+
+
+def uriTest(url)
+
+    uri = URI.parse(url)
+    # debug
+    puts uri.scheme
+
+    # Test proper scheme
+    if uri.scheme.nil? 
+        uri = URI.parse("https://#{url}")
+    end
+
+    return uri
+
+end
+
 options = Optparse.parse(ARGV)
 STDOUT.puts options
+
+uri = uriTest(options.url)
+
+# debug
+puts uri.scheme
+
+response = runTest(options, uri)
+
+case response
+when Net::HTTPSuccess
+  puts response.message
+when Net::HTTPMovedPermanently
+  puts response.message
+when Net::HTTPRedirect
+  #follow_redirect(response)
+else
+  binding.pry
+  raise StandardError, "ERROR: #{response.message}"
+end
